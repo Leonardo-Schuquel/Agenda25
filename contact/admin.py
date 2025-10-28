3218c997df175963f772d6a3eed41edc06fdc6fa
Leonardo Schuquel <leonardoschuquelads@gmail.com>
2025-10-27 21:55:33 -0300

    creating models and configuring

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = '-id',  # Ordena os id de forma decrecente
+    list_filter = ('created_date',)  # Pesquisa por data de criação
+    search_fields = 'id', 'first_name', 'last_name',  # Pesquisa por parametros
+    # Cria paginas e delimita quantos contatos vão aparecer por pagina
+    list_per_page = 50
+    list_max_show_all = 50
+    # Permite editar sem ter que entrar no contato
+    list_editable = 'phone',
+    list_display_links = 'id', 'first_name', 'last_name',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = '-id',  # Ordena os id de forma decrecente
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
+# id (primary key - automático)
 # Create your models here.
+# first_name (string), last_name(string), phone (string),
+# email(email), create_date(date), description(text),
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (image)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Contact(models.Model):
+    # Campos obrigatórios
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+
+    # blank=True torna o campo como opcional
+    email = models.EmailField(max_length=254, blank=True)
+    description = models.TextField(blank=True)
+
+    # Pega de forma automatica
+    created_date = models.DateTimeField(default=timezone.now)
+    show = models.BooleanField(default=True)
+
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
